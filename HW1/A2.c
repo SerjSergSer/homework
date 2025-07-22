d8354f2e8102bdf3e86e959dc7e03a001ed1fd24
Sergei Alexandrov <alexandrov.online@mail.ru>
2025-07-22 19:51:49 +0300

    Добавил А1 и А2 задания

---
+++ HW1/A2.c
@@ -0,0 +1,23 @@
+/*
+    *
+   ***
+  *****
+ *******
+  HHOHH
+  ZZZZZ
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf ("   *\n");
+	printf ("  ***\n");
+	printf (" *****\n");
+	printf ("*******\n");
+	printf (" HHOHH\n");
+	printf (" ZZZZZ\n");
+	return 0;
+}
+
